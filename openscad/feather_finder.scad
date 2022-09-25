77463d9a494fe77fe1af7e2e750306911e210944
Jean-Francois Romang <jromang@users.noreply.github.com>
2022-09-25 19:25:49 +0200

    Initial import

---
+++ openscad/feather_finder.scad
@@ -0,0 +1,66 @@
+//https://github.com/mrWheel/YAPP_Box
+box_height=7.2;
+depth=50;
+battery_size=[37,25.5,5.2];
+featherboard_size=[50.8,22.8,7.2];
+bno055_size=[20.32,25.04,7.2];
+cable_length=40;
+
+include <BOSL2/std.scad>
+include <BOSL2/joiners.scad>
+include<YAPPgenerator_v15.scad>
+
+
+//-- which part do you want to print?
+printBaseShell      = true;
+printLidShell       = true;
+printHolder         = true;
+
+color("gray")
+translate([pcbX,pcbY+battery_size.y+standoffDiameter+2.54/2+1.5 , basePlaneThickness+standoffHeight])
+import("ESP32FeatherV2.stl", convexity=3);
+
+
+//battery
+//#color("gray")
+//translate([pcbX,pcbY,pcbZ])
+//cube(battery_size);
+
+
+bno_position=[pcbX+featherboard_size.x/2,pcbY+battery_size.y+standoffDiameter+2.54+ cable_length+ featherboard_size.y+standoffDiameter-1.5 , basePlaneThickness+standoffHeight];
+color("gray")
+translate(bno_position)
+rotate([0,0,90])
+import("bno055.stl", convexity=3);
+
+difference() {
+YAPPgenerate();
+//vixen finder dovetail
+//https://github.com/revarbat/BOSL2/wiki/joiners.scad#module-dovetail
+translate([pcbX+featherboard_size.x/2,40,0])
+cube([18.5,39,4], center=true);
+//
+}
+
+//holder
+holder_margin=0.2;
+if(printHolder)
+translate([90,110,39.4/2])
+rotate([90,0,0])
+translate([0,0,9])
+difference() {
+    union() {
+    translate([0,-(39-4.2-holder_margin)/2 ,0])   
+    cube([10,4.2,28], center=true);
+    dovetail("female", width=32, height=9, slide=39-holder_margin, angle=30);
+    translate([0,0,49])
+    cube([18.5-holder_margin,39-holder_margin,100], center=true);
+    }
+
+    for (i = [15, 45, 75]){
+        translate([0, 0, i])
+        rotate([0,90,0])
+        cylinder(  30, d=25, center=true, $fn=360);
+    }
+}
+
---
+++ sketch/fsp/fsp.ino
@@ -0,0 +1,180 @@
+/* 
+ * This file is part of the Feather Star Pointer distribution 
+ * (https://github.com/jromang/featherstarpointer).
+ * Copyright (c) 2022 Jean-Francois ROMANG.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <Adafruit_NeoPixel.h>
+#include <BluetoothSerial.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+Adafruit_BNO055 bno = Adafruit_BNO055(55);
+Adafruit_NeoPixel pixel(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
+BluetoothSerial SerialBT;
+
+// Modulo working with negative numbers
+int mod(int a, int b)
+{
+    int r = a % b;
+    return r < 0 ? r + b : r;
+}
+
+// Calibration procedure at startup
+/* 
+  https://learn.adafruit.com/adafruit-bno055-absolute-orientation-sensor/device-calibration
+  To generate valid calibration data, the following criteria should be met:
+
+  Gyroscope: The device must be standing still in any position
+  Magnetometer: In the past 'figure 8' motions were required in 3 dimensions, but with recent devices
+  fast magnetic compensation takes place with sufficient normal movement of the device
+  Accelerometer: The BNO055 must be placed in 6 standing positions for +X, -X, +Y, -Y, +Z and -Z.
+  This is the most onerous sensor to calibrate, but the best solution to generate the calibration data
+  is to find a block of wood or similar object, and place the sensor on each of the 6 'faces' of the block,
+  which will help to maintain sensor alignment during the calibration process.  You should still be able to
+  get reasonable quality data from the BNO055, however, even if the accelerometer isn't entirely
+  or perfectly calibrated.
+*/
+void calibrate()
+{
+  Serial.println("Calibration start");
+  /* Get the four calibration values (0..3) */
+  /* Any sensor data reporting 0 should be ignored, */
+  /* 3 means 'fully calibrated" */
+  uint8_t system, gyro, accel, mag;
+  system = gyro = accel = mag = 0;
+  
+  pixel.setPixelColor(0, 15, 15, 15);
+  pixel.show();
+
+  while(system!=3 || gyro!= 3 || accel!=3 || mag!=3)
+  {
+    bno.getCalibration(&system, &gyro, &accel, &mag);
+    if(system)
+    {
+      pixel.setPixelColor(0,15+gyro*3, 15+accel*3, 15+mag*3);
+      pixel.show();
+
+      /* Display the individual values */
+      Serial.print("Sys:");
+      Serial.print(system, DEC);
+      Serial.print(" G:");
+      Serial.print(gyro, DEC);
+      Serial.print(" A:");
+      Serial.print(accel, DEC);
+      Serial.print(" M:");
+      Serial.println(mag, DEC);
+    }
+    delay(200);
+  }
+  Serial.println("Calibration OK");
+}
+
+
+void setup(void) 
+{
+  // set builtin LED to be an output pin
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+
+  #if defined(NEOPIXEL_POWER)
+    // If this board has a power control pin, we must set it to output and high
+    // in order to enable the NeoPixels. We put this in an #if defined so it can
+    // be reused for other boards without compilation errors
+    pinMode(NEOPIXEL_POWER, OUTPUT);
+    digitalWrite(NEOPIXEL_POWER, HIGH);
+  #endif
+
+  // Turn on the I2C power by pulling pin HIGH.
+  pinMode(NEOPIXEL_I2C_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_I2C_POWER, HIGH);
+  Serial.println("NEOPIXEL_I2C_POWER OK");
+
+  Serial.begin(115200);
+
+  // Wait for Serial port to open
+  while (!Serial) {
+    delay(10);
+  }
+  delay(500);
+  Serial.println("Feather Star Pointer");
+
+  /* Initialise the sensor */
+  if(!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while(1);
+  }
+
+  pixel.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
+  pixel.setBrightness(50); // not so bright
+
+  delay(1000);
+  bno.setExtCrystalUse(true);
+
+  delay(1000);
+  calibrate();
+
+  SerialBT.begin("Feather Star Pointer"); //Bluetooth device name
+
+  pixel.fill(0x00FF00);
+  pixel.show();
+}
+
+
+void loop(void) 
+{
+  if (SerialBT.available()) {
+    char command=SerialBT.read();
+    char response[30];
+    Serial.print(">");
+    Serial.println(command);
+
+    if(command=='Q') 
+    {
+      sensors_event_t event; 
+      bno.getEvent(&event);
+      int az=(int)((event.orientation.x)*8192.0/360.0);
+      int alt=(int)((event.orientation.y)*8192.0/360.0);
+      az=mod(az,8192)-4096;
+      alt=mod(alt,8192)-4096;
+      
+      sprintf(response, "%i %i\n", az, alt);
+      Serial.print("<");
+      Serial.print(response);
+      Serial.println(event.orientation.x);
+      Serial.println(event.orientation.y);
+      SerialBT.println(response);
+      delay(100);
+    }
+    if(command=='H')
+    {
+      snprintf(response, 20, "%u-%u\n", 8192, 8192);
+      Serial.print("<");
+      Serial.print(response);
+      SerialBT.println(response);
+    }
+  }
+}
